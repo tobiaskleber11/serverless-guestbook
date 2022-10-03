0a6d32d741c2a22875ecdd4f2caeaf77c3dfb523
Tobias Kleber <110009209+tobiaskleber11@users.noreply.github.com>
2022-10-03 01:10:26 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-east.functions.appdomain.cloud/api/v1/web/2678d9c3-e564-4de0-b744-7922efd9bb4f/guestbook/read-guestbook-entries-sequence';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
